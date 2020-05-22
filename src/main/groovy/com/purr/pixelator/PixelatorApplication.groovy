63f3e996fe9389b7bcf9190bdbd4f8ff775e0b23
purringlion <judit@tovissy.com>
2020-05-22 21:54:41 +0200

    Init Spring Project

---
+++ src/main/groovy/com/purr/pixelator/PixelatorApplication.groovy
@@ -0,0 +1,13 @@
+package com.purr.pixelator
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PixelatorApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(PixelatorApplication, args)
+	}
+
+}
