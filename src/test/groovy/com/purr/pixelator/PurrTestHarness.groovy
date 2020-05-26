7e0438a5b87f89ede8362a967cc7675a6d64328b
purringlion <judit@tovissy.com>
2020-05-26 22:30:52 +0200

    Started filling out test harness

--- src/test/groovy/com/purr/pixelator/PurrTestHarness.groovy
+++ src/test/groovy/com/purr/pixelator/PurrTestHarness.groovy
@@ -3,4 +3,8 @@ package com.purr.pixelator
 import spock.lang.Specification
 
 class PurrTestHarness extends Specification {
+
+    void tellMeEverything(Exception e) {
+        //todo print everything worth knowing about an exception here
+    }
 }
