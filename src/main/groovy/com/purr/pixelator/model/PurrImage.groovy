6a395d14ef8f3abd07c1328dce11459525edc9d5
purringlion <judit@tovissy.com>
2020-05-26 20:23:36 +0200

    RGBPixel model added

---
+++ src/main/groovy/com/purr/pixelator/model/PurrImage.groovy
@@ -0,0 +1,13 @@
+package com.purr.pixelator.model
+
+import java.awt.image.BufferedImage
+
+class PurrImage {
+
+    private BufferedImage img
+
+    PurrImage(BufferedImage img) {
+        this.img = img
+    }
+
+}
---
+++ src/main/groovy/com/purr/pixelator/model/RGBPixel.groovy
@@ -0,0 +1,46 @@
+package com.purr.pixelator.model
+
+/**
+ * This class uses a 32 bit XRGB representation
+ */
+class RGBPixel {
+
+    protected final static int BITMASK_RED = 16711680;   // 00000000111111110000000000000000
+    protected final static int BITMASK_GREEN = 65280;    // 00000000000000001111111100000000
+    protected final static int BITMASK_BLUE = 255;       // 00000000000000000000000011111111
+
+    private int pixelValue
+
+    int getRed(){
+        return  (pixelValue & BITMASK_RED) >>16
+    }
+
+    int getGreen(){
+        return (pixelValue & BITMASK_GREEN)>>8
+    }
+
+    int getBlue(){
+        return pixelValue & BITMASK_BLUE
+    }
+
+    int getPixelValue() {
+        return pixelValue
+    }
+
+    void setPixelValue(int pixelValue) {
+        this.pixelValue = pixelValue
+    }
+
+    RGBPixel() {
+        this(0)
+    }
+
+    RGBPixel(int pixelValue) {
+        this.pixelValue = pixelValue
+    }
+
+    RGBPixel(int r, int g, int b) {
+        pixelValue = ((r << 16) | (g << 8) | (b))
+    }
+
+}
