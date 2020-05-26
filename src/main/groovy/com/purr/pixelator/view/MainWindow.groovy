8d4c23ba42925be710bd0ce67a888b37b3df0f86
purringlion <judit@tovissy.com>
2020-05-26 16:06:24 +0200

    MainWindow properties moved to class vars

--- src/main/groovy/com/purr/pixelator/view/MainWindow.groovy
+++ src/main/groovy/com/purr/pixelator/view/MainWindow.groovy
@@ -1,7 +1,6 @@
 package com.purr.pixelator.view
 
 import groovy.util.logging.Slf4j
-
 import javax.swing.JFrame
 import java.awt.GridLayout
 import java.awt.HeadlessException
@@ -9,16 +8,23 @@ import java.awt.HeadlessException
 @Slf4j
 class MainWindow extends JFrame {
 
+	//TODO get these from properties
+	private final static String windowTitle = "Pixelator by Judit Tovissy @ tovissy.com"
+	private final static int windowSizeX = 500
+	private final static int windowSizeY = 525
+	private final static int gridRows = 2
+	private final static int gridCols = 1
+
+
     MainWindow() throws HeadlessException {
         log.trace("MEOW :: MainWindow Init")
 		setupWindow()
     }
 
 	private setupWindow() {
-		setLayout(new GridLayout(2,1))
-		this.setTitle("Pixelator by Judit Tovissy @ tovissy.com")
-		setSize(500,525)
+		setLayout(new GridLayout(gridRows,gridCols))
+		this.setTitle(windowTitle)
+		setSize(windowSizeX, windowSizeY)
 		setDefaultCloseOperation(EXIT_ON_CLOSE)
-//		setVisible(true) // I'll set this on App level
 	}
 }
